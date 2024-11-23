400c49dd85d60e8154623fc1e6b42471eb494c5d
sertanrdn <sertanerdogan07@gmail.com>
2024-11-23 19:13:22 +0100

    Updated traffic-light-2.js file

--- Week1/prep-exercises/1-traffic-light/traffic-light-2.js
+++ Week1/prep-exercises/1-traffic-light/traffic-light-2.js
@@ -13,11 +13,11 @@ let cycle = 0;
 while (cycle < 2) {
   const currentState = trafficLight.possibleStates[trafficLight.stateIndex];
   console.log("The traffic light is on", currentState);
-  if (trafficLight.possibleStates[trafficLight.stateIndex] === "green") {
+  if (currentState === "green") {
     trafficLight.stateIndex = 1; // Turn to orange
-  } else if (trafficLight.possibleStates[trafficLight.stateIndex] === "orange") {
+  } else if (currentState === "orange") {
     trafficLight.stateIndex = 2; // Turn to red
-  } else if (trafficLight.possibleStates[trafficLight.stateIndex] === "red") {
+  } else if (currentState === "red") {
     cycle++;
     trafficLight.stateIndex = 0; // Turn to green again
   }
